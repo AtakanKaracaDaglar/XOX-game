7359ee5e70f1195cb9228dc118368524ef061530
Atakan Karaca Dağlar <atakansansaman@gmail.com>
2025-03-17 18:57:11 +0300

    Add project files.

---
+++ XOX-game/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace XOX_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.x_pct = new System.Windows.Forms.PictureBox();
+            this.o_pct = new System.Windows.Forms.PictureBox();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.x_ = new System.Windows.Forms.PictureBox();
+            this.o_ = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.x_pct)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.o_pct)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x_)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.o_)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "---------------------------------------------------------------------------------" +
+    "---------------------";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(415, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "---------------------------------------------------------------------------------" +
+    "---------------------";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(362, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 496);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n" +
+    "|\r\n|\r\n\r\n\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(197, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 480);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n|\r\n" +
+    "|\r\n|\r\n\r\n";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(82, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(109, 101);
+            this.panel1.TabIndex = 4;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Location = new System.Drawing.Point(226, 45);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(109, 101);
+            this.panel2.TabIndex = 5;
+            this.panel2.Click += new System.EventHandler(this.panel2_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Location = new System.Drawing.Point(385, 45);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(109, 101);
+            this.panel3.TabIndex = 6;
+            this.panel3.Click += new System.EventHandler(this.panel3_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Transparent;
+            this.panel4.Location = new System.Drawing.Point(385, 192);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(109, 101);
+            this.panel4.TabIndex = 6;
+            this.panel4.Click += new System.EventHandler(this.panel4_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Transparent;
+            this.panel5.Location = new System.Drawing.Point(226, 192);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(109, 101);
+            this.panel5.TabIndex = 6;
+            this.panel5.Click += new System.EventHandler(this.panel5_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Transparent;
+            this.panel6.Location = new System.Drawing.Point(82, 192);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(109, 101);
+            this.panel6.TabIndex = 6;
+            this.panel6.Click += new System.EventHandler(this.panel6_Click);
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Transparent;
+            this.panel7.Location = new System.Drawing.Point(82, 349);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(109, 101);
+            this.panel7.TabIndex = 6;
+            this.panel7.Click += new System.EventHandler(this.panel7_Click);
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Transparent;
+            this.panel8.Location = new System.Drawing.Point(226, 349);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(109, 101);
+            this.panel8.TabIndex = 6;
+            this.panel8.Click += new System.EventHandler(this.panel8_Click);
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.Transparent;
+            this.panel9.Location = new System.Drawing.Point(385, 349);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(109, 101);
+            this.panel9.TabIndex = 6;
+            this.panel9.Click += new System.EventHandler(this.panel9_Click);
+            // 
+            // x_pct
+            // 
+            this.x_pct.Image = ((System.Drawing.Image)(resources.GetObject("x_pct.Image")));
+            this.x_pct.Location = new System.Drawing.Point(628, 192);
+            this.x_pct.Name = "x_pct";
+            this.x_pct.Size = new System.Drawing.Size(103, 95);
+            this.x_pct.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.x_pct.TabIndex = 8;
+            this.x_pct.TabStop = false;
+            this.x_pct.Tag = "x_harfi";
+            this.x_pct.Click += new System.EventHandler(this.x_pct_Click);
+            // 
+            // o_pct
+            // 
+            this.o_pct.Image = ((System.Drawing.Image)(resources.GetObject("o_pct.Image")));
+            this.o_pct.Location = new System.Drawing.Point(628, 51);
+            this.o_pct.Name = "o_pct";
+            this.o_pct.Size = new System.Drawing.Size(103, 95);
+            this.o_pct.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.o_pct.TabIndex = 8;
+            this.o_pct.TabStop = false;
+            this.o_pct.Tag = "o_harfi";
+            this.o_pct.Click += new System.EventHandler(this.o_pct_Click);
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(607, 332);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(141, 72);
+            this.start_btn.TabIndex = 9;
+            this.start_btn.Text = "Oyunu Başlat";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // x_
+            // 
+            this.x_.Image = ((System.Drawing.Image)(resources.GetObject("x_.Image")));
+            this.x_.Location = new System.Drawing.Point(832, 192);
+            this.x_.Name = "x_";
+            this.x_.Size = new System.Drawing.Size(103, 95);
+            this.x_.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.x_.TabIndex = 8;
+            this.x_.TabStop = false;
+            this.x_.Tag = "x_harfi";
+            // 
+            // o_
+            // 
+            this.o_.Image = ((System.Drawing.Image)(resources.GetObject("o_.Image")));
+            this.o_.Location = new System.Drawing.Point(832, 51);
+            this.o_.Name = "o_";
+            this.o_.Size = new System.Drawing.Size(103, 95);
+            this.o_.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.o_.TabIndex = 8;
+            this.o_.TabStop = false;
+            this.o_.Tag = "o_harfi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 553);
+            this.Controls.Add(this.x_);
+            this.Controls.Add(this.start_btn);
+            this.Controls.Add(this.o_);
+            this.Controls.Add(this.o_pct);
+            this.Controls.Add(this.x_pct);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.x_pct)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.o_pct)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x_)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.o_)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.PictureBox x_pct;
+        private System.Windows.Forms.PictureBox o_pct;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.PictureBox x_;
+        private System.Windows.Forms.PictureBox o_;
+    }
+}
+
---
+++ XOX-game/Form1.cs
@@ -0,0 +1,513 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XOX_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tahtayiTemizle();
+        }
+
+        private int baslayan;
+        private bool oyuncuTiklayabilirMi;
+        private int siraKimde; // 0 / 1
+        private bool secimSirasiMi = false;
+        
+        private int baslayaniSec() // 0 Yada 1 döndürüyor | 0 Bilgisayar 1 Oyuncudur
+        {
+            int baslayanSecimi;
+            
+            Random atakan = new Random();
+            baslayanSecimi = atakan.Next(0, 2);
+            
+            if(baslayanSecimi == 0)
+            {
+                MessageBox.Show("Başlayan Bilgisayar Seçildi");
+            } else if(baslayanSecimi == 1)
+            {
+                MessageBox.Show("Başlayan Oyuncu Seçildi");
+                MessageBox.Show("Lütfen Taş Türünü Seçiniz");
+                secimSirasiMi = true;
+            }
+            
+            siraKimde = baslayanSecimi;
+
+            return baslayanSecimi;
+        }
+
+
+
+        int[,] atakan = new int[3, 3];
+        
+        private void tahtayiTemizle()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                for (int a = 0; a < 3; a++)
+                {
+                    atakan[a,i] = 5;
+                }
+            }
+        }
+
+
+        //    1 = X   0 = O   \\
+        private bool yatayTespit;
+        private bool dikeyTespit;
+        private bool caprazTespit;
+
+        private int yatayTespitSonucu;
+        private int dikeyTespitSonucu;
+        private int caprazTespitSonucu;
+
+        private void yatayTarama()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                int a = atakan[row, 0];
+                int b = atakan[row, 1];
+                int c = atakan[row, 2];
+
+                if (a == b && b == c && a != 5)
+                {
+                    yatayTespit = true;
+                    yatayTespitSonucu = a;
+                    return; // Tespit edildi, çık
+                }
+            }
+            yatayTespit = false;
+        }
+
+
+        // Çapraz Kontrol Mekanizmasi
+
+        private void caprazTarama()
+        {
+            int a = atakan[0, 0];
+            int b = atakan[1, 1];
+            int c = atakan[2, 2];
+
+            if (a == b && b == c && a != 5)
+            {
+                caprazTespit = true;
+                caprazTespitSonucu = a;
+                return;
+            }
+
+            a = atakan[0, 2];
+            b = atakan[1, 1];
+            c = atakan[2, 0];
+
+            if (a == b && b == c && a != 5)
+            {
+                caprazTespit = true;
+                caprazTespitSonucu = a;
+            }
+            else
+            {
+                caprazTespit = false;
+            }
+        }
+
+
+
+        private void dikeyTarama()
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                int a = atakan[0, col];
+                int b = atakan[1, col];
+                int c = atakan[2, col];
+
+                if (a == b && b == c && a != 5)
+                {
+                    dikeyTespit = true;
+                    dikeyTespitSonucu = a;
+                    return;
+                }
+            }
+            dikeyTespit = false;
+        }
+
+
+
+
+
+
+        // OYUNUN BEL KEMİGİ <3 \\
+
+
+        private int oynanmaSayisi = 0; // Lütfen oynanma sayisini sürekli sıfırlamayı unutmayınız <3 XD
+
+        //    1 = X   0 = O   \\
+
+        private int secilenKarakter = 2; // Oyuncunun Oynadığı taş
+
+
+        private void clooner(PictureBox atakan, Point lokasyon)
+        {
+            PictureBox kopyalaniyor = new PictureBox();
+
+            kopyalaniyor.Image = atakan.Image;
+            kopyalaniyor.Location = lokasyon;
+            kopyalaniyor.Size = atakan.Size;
+            kopyalaniyor.SizeMode = atakan.SizeMode;
+            kopyalaniyor.Visible = true;
+
+            this.Controls.Add(kopyalaniyor);
+            kopyalaniyor.BringToFront();
+            }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if(secimSirasiMi == true)
+            {
+                return;
+            } else
+            {
+                if (siraKimde == 0 && oynanmaSayisi == 0)
+                {
+                    // Sıra Bilgisayarda
+
+                    siraKimde = 1; // sıra oyuncuya geçti
+                    oynanmaSayisi = 1;
+                    MessageBox.Show("Hamle Sırası Oyuncuya Devrediliyor..");
+
+                }
+                else if (siraKimde == 1 && oynanmaSayisi == 0)
+                {
+                    // Sıra Oyuncuda
+                    
+
+                    oynanmaSayisi = 1;
+                    MessageBox.Show("Sira Sizde");
+                    //  siraKimde = 0;
+                }
+
+
+
+
+            }
+
+
+            // SÜREKLİ ÇALIŞACAK OLAN TARAMA İŞLEMLERİ \\
+
+            dikeyTarama();
+            yatayTarama();
+            caprazTarama();
+
+            if(yatayTespit == true)
+            {
+                Console.WriteLine("Yatay TESPİT EDİLDİ");
+            }
+            if(dikeyTespit  == true)
+            {
+                Console.WriteLine("Dikey TESPİT EDİLDİ");
+            }
+            if (caprazTespit  == true)
+            {
+                Console.WriteLine("Capraz TESPİT EDİLDİ");
+            }
+
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            baslayaniSec();
+            timer1.Start();
+        }
+
+        private void x_pct_Click(object sender, EventArgs e)
+        {
+            if(secimSirasiMi == true)
+            {
+                secilenKarakter = 1; // X harfi
+                x_pct.Visible = false;
+                o_pct.Visible = false;
+                secimSirasiMi = false;
+            }
+        }
+
+        private void o_pct_Click(object sender, EventArgs e)
+        {
+            if (secimSirasiMi == true)
+            {
+                secilenKarakter = 0; // Y harfi
+                o_pct.Visible = false;
+                x_pct.Visible = false;
+                secimSirasiMi = false;
+            }
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            if (siraKimde == 1)
+            {
+                if(secilenKarakter == 1)
+                {
+
+                    clooner(x_, new Point(panel1.Location.X, panel1.Location.Y));
+                    atakan[0,0] = 1;
+                }
+                else if (secilenKarakter == 0)
+                {
+                    clooner(o_, new Point(panel1.Location.X, panel1.Location.Y));
+                    atakan[0, 0] = 0;
+                }   
+                
+
+
+                siraKimde = 0;
+                oynanmaSayisi = 0;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Sıra bilgisayarda");
+            }
+        }
+
+        private void panel2_Click(object sender, EventArgs e)
+        {
+            if (siraKimde == 1)
+            {
+                if (secilenKarakter == 1)
+                {
+                    clooner(x_, new Point(panel2.Location.X, panel2.Location.Y));
+                    atakan[0,1] = 1;
+                }
+                else if (secilenKarakter == 0)
+                {
+                    clooner(o_, new Point(panel2.Location.X, panel2.Location.Y));
+                    atakan[0, 1] = 0;
+                }
+
+
+
+                siraKimde = 0;
+                oynanmaSayisi = 0;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Sıra bilgisayarda");
+            }
+        }
+
+        private void panel3_Click(object sender, EventArgs e)
+        {
+            if (siraKimde == 1)
+            {
+                if (secilenKarakter == 1)
+                {
+
+                    clooner(x_, new Point(panel3.Location.X, panel3.Location.Y));
+                    atakan[0,2] = 1;
+                }
+                else if (secilenKarakter == 0)
+                {
+                    clooner(o_, new Point(panel3.Location.X, panel3.Location.Y));
+                    atakan[0, 2] = 0;
+                }
+
+
+
+                siraKimde = 0;
+                oynanmaSayisi = 0;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Sıra bilgisayarda");
+            }
+        }
+
+
+
+
+
+        private void panel6_Click(object sender, EventArgs e)
+        {
+            if (siraKimde == 1)
+            {
+                if (secilenKarakter == 1)
+                {
+
+                    clooner(x_, new Point(panel6.Location.X, panel6.Location.Y));
+                    atakan[1, 0] = 1;
+                }
+                else if (secilenKarakter == 0)
+                {
+                    clooner(o_, new Point(panel6.Location.X, panel6.Location.Y));
+                    atakan[1, 0] = 0;
+                }
+
+
+
+                siraKimde = 0;
+                oynanmaSayisi = 0;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Sıra bilgisayarda");
+            }
+        }
+
+        private void panel5_Click(object sender, EventArgs e)
+        {
+            if (siraKimde == 1)
+            {
+                if (secilenKarakter == 1)
+                {
+                    clooner(x_, new Point(panel5.Location.X, panel5.Location.Y));
+                    atakan[1, 1]=1;
+                }
+                else if (secilenKarakter == 0)
+                {
+                    clooner(o_, new Point(panel5.Location.X, panel5.Location.Y));
+                    atakan[1, 1] = 0;
+                }
+
+
+
+                siraKimde = 0;
+                oynanmaSayisi = 0;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Sıra bilgisayarda");
+            }
+        }
+
+        private void panel4_Click(object sender, EventArgs e)
+        {
+            if (siraKimde == 1)
+            {
+                if (secilenKarakter == 1)
+                {
+                    clooner(x_, new Point(panel4.Location.X, panel4.Location.Y));
+                    atakan[1, 2] = 1;
+                }
+                else if (secilenKarakter == 0)
+                {
+                    clooner(o_, new Point(panel4.Location.X, panel4.Location.Y));
+                    atakan[1, 2] = 0;
+                }
+
+
+
+                siraKimde = 0;
+                oynanmaSayisi = 0;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Sıra bilgisayarda");
+            }
+        }
+
+
+
+
+
+
+
+        private void panel7_Click(object sender, EventArgs e)
+        {
+            if (siraKimde == 1)
+            {
+                if (secilenKarakter == 1)
+                {
+                    clooner(x_, new Point(panel7.Location.X, panel7.Location.Y));
+                    atakan[2, 0] = 1;
+                }
+                else if (secilenKarakter == 0)
+                {
+                    clooner(o_, new Point(panel7.Location.X, panel7.Location.Y));
+                    atakan[2, 0] = 0;
+                }
+
+
+
+                siraKimde = 0;
+                oynanmaSayisi = 0;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Sıra bilgisayarda");
+            }
+        }
+
+        private void panel8_Click(object sender, EventArgs e)
+        {
+            if (siraKimde == 1)
+            {
+                if (secilenKarakter == 1)
+                {
+                    clooner(x_, new Point(panel8.Location.X, panel8.Location.Y));
+                    atakan[2, 1] = 1;
+                }
+                else if (secilenKarakter == 0)
+                {
+                    clooner(o_, new Point(panel8.Location.X, panel8.Location.Y));
+                    atakan[2, 1] = 0;
+                }
+
+
+
+                siraKimde = 0;
+                oynanmaSayisi = 0;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Sıra bilgisayarda");
+            }
+        }
+
+        private void panel9_Click(object sender, EventArgs e)
+        {
+            if (siraKimde == 1)
+            {
+                if (secilenKarakter == 1)
+                {
+                    clooner(x_, new Point(panel9.Location.X, panel9.Location.Y));
+                    atakan[2, 2] = 1;
+                }
+                else if (secilenKarakter == 0)
+                {
+                    clooner(o_, new Point(panel9.Location.X, panel9.Location.Y));
+                    atakan[2, 2] = 0;
+                }
+
+
+
+                siraKimde = 0;
+                oynanmaSayisi = 0;
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Sıra bilgisayarda");
+            }
+        }
+    }
+}
