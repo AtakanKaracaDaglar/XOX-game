0bf2258576f8c10ab6bc558fc00218bbd75d271d
Atakan Karaca Dağlar <atakansansaman@gmail.com>
2025-03-19 00:22:31 +0300

    Oyun stabil hale getirildi dahada Guncelleme devam edecek.

--- XOX-game/Form1.cs
+++ XOX-game/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace XOX_game
@@ -26,24 +21,33 @@ namespace XOX_game
         private bool oyuncuTiklayabilirMi;
         private int siraKimde; // 0 / 1
         private bool secimSirasiMi = false;
-        
+
         private int baslayaniSec() // 0 Yada 1 döndürüyor | 0 Bilgisayar 1 Oyuncudur
         {
             int baslayanSecimi;
-            
+
             Random atakan = new Random();
             baslayanSecimi = atakan.Next(0, 2);
-            
-            if(baslayanSecimi == 0)
+
+            if (baslayanSecimi == 0)
             {
                 MessageBox.Show("Başlayan Bilgisayar Seçildi");
-            } else if(baslayanSecimi == 1)
+                //bilgisayar rastgele karakter seçecek
+                int bilgisayarKarakter = atakan.Next(0, 2);
+
+                int oyuncuKarakter = (bilgisayarKarakter == 0) ? 1 : 0;
+
+                secilenKarakter = oyuncuKarakter; //Oyuncuya kalan karakteri atıyoruz
+                siraKimde = 0; //ilk hamle bilgisayarın olacak
+
+            }
+            else if (baslayanSecimi == 1)
             {
                 MessageBox.Show("Başlayan Oyuncu Seçildi");
                 MessageBox.Show("Lütfen Taş Türünü Seçiniz");
                 secimSirasiMi = true;
             }
-            
+
             siraKimde = baslayanSecimi;
 
             return baslayanSecimi;
@@ -52,14 +56,14 @@ namespace XOX_game
 
 
         int[,] atakan = new int[3, 3];
-        
+
         private void tahtayiTemizle()
         {
-            for(int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
                 for (int a = 0; a < 3; a++)
                 {
-                    atakan[a,i] = 5;
+                    atakan[a, i] = 5;
                 }
             }
         }
@@ -170,71 +174,268 @@ namespace XOX_game
 
             this.Controls.Add(kopyalaniyor);
             kopyalaniyor.BringToFront();
+        }
+
+
+
+
+        //yapay zeka yönetimi
+        private void AImove()
+        {
+            const int O = 0;
+            const int X = 1;
+
+            int AIsymbol = (secilenKarakter == 1) ? 0 : 1;
+
+            //kazanmayı dene
+            if (TryWinningMove(AIsymbol))
+            {
+                sonrakiHamleGec();
+                return;
+            }
+            //rakibin kazanmasını engelleme
+            if (TryWinningMove(secilenKarakter, true))
+            {
+                sonrakiHamleGec();
+                return;
+            }
+            //yoksa rastgele oyna
+            RandomHareket(AIsymbol);
+            sonrakiHamleGec();
+
+
+        }
+
+
+        private bool TryWinningMove(int symbol, bool engellemeModu = false)
+        {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    if (atakan[r, c] == 5) // boş hücre
+                    {
+                        atakan[r, c] = symbol;
+
+                        if (CheckWinTemp(symbol))
+                        {
+                            atakan[r, c] = 5; // geri al
+                            if (!engellemeModu)
+                            {
+                                // AI'nin kazanma hamlesi: yerleştir
+                                atakan[r, c] = symbol;
+                                SembolYerlestirme(r, c, symbol);
+                            }
+                            else
+                            {
+                                // Engellemek için, AI'nin taşını koy
+                                int aiSymbol = (secilenKarakter == 1) ? 0 : 1;
+                                atakan[r, c] = aiSymbol;
+                                SembolYerlestirme(r, c, aiSymbol);
+                            }
+                            return true;
+                        }
+
+                        atakan[r, c] = 5; // geri al
+                    }
+                }
+            }
+            return false;
+        }
+
+
+
+
+
+        private bool CheckWinTemp(int symbol)
+        {
+            // Geçici tarama bayrakları
+            bool tempYatay = false, tempDikey = false, tempCapraz = false;
+
+            // Yatay
+            for (int row = 0; row < 3; row++)
+            {
+                if (atakan[row, 0] == symbol && atakan[row, 1] == symbol && atakan[row, 2] == symbol)
+                {
+                    tempYatay = true;
+                    break;
+                }
             }
 
-        private void timer1_Tick(object sender, EventArgs e)
+            // Dikey
+            for (int col = 0; col < 3; col++)
+            {
+                if (atakan[0, col] == symbol && atakan[1, col] == symbol && atakan[2, col] == symbol)
+                {
+                    tempDikey = true;
+                    break;
+                }
+            }
+
+            // Çapraz
+            if ((atakan[0, 0] == symbol && atakan[1, 1] == symbol && atakan[2, 2] == symbol) ||
+                (atakan[0, 2] == symbol && atakan[1, 1] == symbol && atakan[2, 0] == symbol))
+            {
+                tempCapraz = true;
+            }
+
+            return tempYatay || tempDikey || tempCapraz;
+        }
+
+        private void RandomHareket(int symbol)
         {
 
-            if(secimSirasiMi == true)
+            List<(int r, int c)> boslist = new List<(int r, int c)>();
+
+            for (int r = 0; r < 3; r++)
             {
-                return;
-            } else
+                for (int c = 0; c < 3; c++)
+                {
+                    if (atakan[r, c] == 5)
+                    {
+                        boslist.Add((r, c));
+                    }
+                }
+            }
+
+            if (boslist.Count > 0)
             {
-                if (siraKimde == 0 && oynanmaSayisi == 0)
-                {
-                    // Sıra Bilgisayarda
+                Random rnd = new Random();
 
-                    siraKimde = 1; // sıra oyuncuya geçti
-                    oynanmaSayisi = 1;
-                    MessageBox.Show("Hamle Sırası Oyuncuya Devrediliyor..");
+                var (rr, cc) = boslist[rnd.Next(boslist.Count)];
 
-                }
-                else if (siraKimde == 1 && oynanmaSayisi == 0)
-                {
-                    // Sıra Oyuncuda
-                    
+                atakan[rr, cc] = symbol;
 
-                    oynanmaSayisi = 1;
-                    MessageBox.Show("Sira Sizde");
-                    //  siraKimde = 0;
-                }
-
-
-
+                SembolYerlestirme(rr, cc, symbol);
 
             }
 
+        }
 
-            // SÜREKLİ ÇALIŞACAK OLAN TARAMA İŞLEMLERİ \\
+        private void SembolYerlestirme(int row, int col, int symbol)
+        {
 
+            PictureBox picture = (symbol == 0) ? o_ : x_;
+
+            Panel hedefPanel = GetPanelByRowCol(row, col);
+
+            clooner(picture, new Point(hedefPanel.Location.X, hedefPanel.Location.Y));
+
+        }
+
+
+        private Panel GetPanelByRowCol(int r, int c)
+        {
+            if (r == 0 && c == 0) return panel1;
+            if (r == 0 && c == 1) return panel2;
+            if (r == 0 && c == 2) return panel3;
+            if (r == 1 && c == 0) return panel6;
+            if (r == 1 && c == 1) return panel5;
+            if (r == 1 && c == 2) return panel4;
+            if (r == 2 && c == 0) return panel7;
+            if (r == 2 && c == 1) return panel8;
+            if (r == 2 && c == 2) return panel9;
+            return null; // güvenlik amaçlı
+        }
+
+
+        private void sonrakiHamleGec()
+        {
+            siraKimde = 1;
+            oynanmaSayisi = 0;
+
+        }
+
+
+        private bool KazananKontrolEt()
+        {
+            yatayTespit = false;
+            dikeyTespit = false;
+            caprazTespit = false;
+
+            // TARAMA
             dikeyTarama();
             yatayTarama();
             caprazTarama();
 
-            if(yatayTespit == true)
+            if (yatayTespit || dikeyTespit || caprazTespit)
             {
-                Console.WriteLine("Yatay TESPİT EDİLDİ");
-            }
-            if(dikeyTespit  == true)
-            {
-                Console.WriteLine("Dikey TESPİT EDİLDİ");
-            }
-            if (caprazTespit  == true)
-            {
-                Console.WriteLine("Capraz TESPİT EDİLDİ");
+                timer1.Stop();
+                int kazanan = yatayTespit ? yatayTespitSonucu : dikeyTespit ? dikeyTespitSonucu : caprazTespitSonucu;
+                string kazananSatir = kazanan == secilenKarakter ? "Oyuncu Kazandı!" : "Bilgisayar Kazandı!";
+                MessageBox.Show(kazananSatir);
+
+                DialogResult dr = MessageBox.Show("Oyunu yeniden başlatmak ister misiniz?", "Yeniden Başlat", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)
+                {
+                    oyunuSifirla();
+                }
+                return true; // Oyun bitti
             }
 
+            return false; // Oyun devam ediyor
         }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (secimSirasiMi == true)
+                return;
+
+            if (siraKimde == 0 && oynanmaSayisi == 0)
+            {
+                AImove();
+            }
+            else if (siraKimde == 1 && oynanmaSayisi == 0)
+            {
+                oynanmaSayisi = 1;
+                MessageBox.Show("Sira Sizde");
+            }
+
+            KazananKontrolEt();
+        }
+
 
         private void start_btn_Click(object sender, EventArgs e)
         {
+
+            
             baslayaniSec();
+            timer1.Start();
+
+        }
+
+
+        private void oyunuSifirla()
+        {
+            tahtayiTemizle();
+            secimSirasiMi = false;
+            oynanmaSayisi = 0;
+
+            // Panel üzerindeki PictureBox'ları temizle
+            List<Control> silinecekler = new List<Control>();
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is PictureBox && (ctrl != x_ && ctrl != o_))  // Orijinal x_ ve o_ hariç
+                {
+                    silinecekler.Add(ctrl);
+                }
+            }
+            foreach (var item in silinecekler)
+            {
+                this.Controls.Remove(item);
+                item.Dispose();
+            }
+
             timer1.Start();
         }
 
+
+
+
         private void x_pct_Click(object sender, EventArgs e)
         {
-            if(secimSirasiMi == true)
+            if (secimSirasiMi == true)
             {
                 secilenKarakter = 1; // X harfi
                 x_pct.Visible = false;
@@ -258,23 +459,32 @@ namespace XOX_game
         {
             if (siraKimde == 1)
             {
-                if(secilenKarakter == 1)
+
+                if (atakan[0, 0] != 5)
+                {
+                    MessageBox.Show("Bu hücre zaten dolu!");
+                    return;
+                }
+
+
+                if (secilenKarakter == 1)
                 {
 
                     clooner(x_, new Point(panel1.Location.X, panel1.Location.Y));
-                    atakan[0,0] = 1;
+                    atakan[0, 0] = 1;
                 }
                 else if (secilenKarakter == 0)
                 {
                     clooner(o_, new Point(panel1.Location.X, panel1.Location.Y));
                     atakan[0, 0] = 0;
-                }   
-                
+                }
+
 
 
                 siraKimde = 0;
                 oynanmaSayisi = 0;
                 timer1.Start();
+                KazananKontrolEt();
             }
             else
             {
@@ -286,10 +496,18 @@ namespace XOX_game
         {
             if (siraKimde == 1)
             {
+
+
+                if (atakan[0, 1] != 5)
+                {
+                    MessageBox.Show("Bu hücre zaten dolu!");
+                    return;
+                }
+
                 if (secilenKarakter == 1)
                 {
                     clooner(x_, new Point(panel2.Location.X, panel2.Location.Y));
-                    atakan[0,1] = 1;
+                    atakan[0, 1] = 1;
                 }
                 else if (secilenKarakter == 0)
                 {
@@ -302,6 +520,7 @@ namespace XOX_game
                 siraKimde = 0;
                 oynanmaSayisi = 0;
                 timer1.Start();
+                KazananKontrolEt();
             }
             else
             {
@@ -313,11 +532,19 @@ namespace XOX_game
         {
             if (siraKimde == 1)
             {
+
+                if (atakan[0, 2] != 5)
+                {
+                    MessageBox.Show("Bu hücre zaten dolu!");
+                    return;
+                }
+
+
                 if (secilenKarakter == 1)
                 {
 
                     clooner(x_, new Point(panel3.Location.X, panel3.Location.Y));
-                    atakan[0,2] = 1;
+                    atakan[0, 2] = 1;
                 }
                 else if (secilenKarakter == 0)
                 {
@@ -330,6 +557,7 @@ namespace XOX_game
                 siraKimde = 0;
                 oynanmaSayisi = 0;
                 timer1.Start();
+                KazananKontrolEt();
             }
             else
             {
@@ -345,6 +573,14 @@ namespace XOX_game
         {
             if (siraKimde == 1)
             {
+
+
+                if (atakan[1, 0] != 5)
+                {
+                    MessageBox.Show("Bu hücre zaten dolu!");
+                    return;
+                }
+
                 if (secilenKarakter == 1)
                 {
 
@@ -362,6 +598,7 @@ namespace XOX_game
                 siraKimde = 0;
                 oynanmaSayisi = 0;
                 timer1.Start();
+                KazananKontrolEt();
             }
             else
             {
@@ -373,10 +610,18 @@ namespace XOX_game
         {
             if (siraKimde == 1)
             {
+
+                if (atakan[1, 1] != 5)
+                {
+                    MessageBox.Show("Bu hücre zaten dolu!");
+                    return;
+                }
+
+
                 if (secilenKarakter == 1)
                 {
                     clooner(x_, new Point(panel5.Location.X, panel5.Location.Y));
-                    atakan[1, 1]=1;
+                    atakan[1, 1] = 1;
                 }
                 else if (secilenKarakter == 0)
                 {
@@ -389,6 +634,7 @@ namespace XOX_game
                 siraKimde = 0;
                 oynanmaSayisi = 0;
                 timer1.Start();
+                KazananKontrolEt();
             }
             else
             {
@@ -400,6 +646,14 @@ namespace XOX_game
         {
             if (siraKimde == 1)
             {
+
+                if (atakan[1, 2] != 5)
+                {
+                    MessageBox.Show("Bu hücre zaten dolu!");
+                    return;
+                }
+
+
                 if (secilenKarakter == 1)
                 {
                     clooner(x_, new Point(panel4.Location.X, panel4.Location.Y));
@@ -416,6 +670,7 @@ namespace XOX_game
                 siraKimde = 0;
                 oynanmaSayisi = 0;
                 timer1.Start();
+                KazananKontrolEt();
             }
             else
             {
@@ -435,6 +690,14 @@ namespace XOX_game
             {
                 if (secilenKarakter == 1)
                 {
+
+                    if (atakan[2, 0] != 5)
+                    {
+                        MessageBox.Show("Bu hücre zaten dolu!");
+                        return;
+                    }
+
+
                     clooner(x_, new Point(panel7.Location.X, panel7.Location.Y));
                     atakan[2, 0] = 1;
                 }
@@ -449,6 +712,7 @@ namespace XOX_game
                 siraKimde = 0;
                 oynanmaSayisi = 0;
                 timer1.Start();
+                KazananKontrolEt();
             }
             else
             {
@@ -460,6 +724,15 @@ namespace XOX_game
         {
             if (siraKimde == 1)
             {
+
+
+                if (atakan[2, 1] != 5)
+                {
+                    MessageBox.Show("Bu hücre zaten dolu!");
+                    return;
+                }
+
+
                 if (secilenKarakter == 1)
                 {
                     clooner(x_, new Point(panel8.Location.X, panel8.Location.Y));
@@ -476,6 +749,7 @@ namespace XOX_game
                 siraKimde = 0;
                 oynanmaSayisi = 0;
                 timer1.Start();
+                KazananKontrolEt();
             }
             else
             {
@@ -487,6 +761,14 @@ namespace XOX_game
         {
             if (siraKimde == 1)
             {
+
+                if (atakan[2, 2] != 5)
+                {
+                    MessageBox.Show("Bu hücre zaten dolu!");
+                    return;
+                }
+
+
                 if (secilenKarakter == 1)
                 {
                     clooner(x_, new Point(panel9.Location.X, panel9.Location.Y));
@@ -503,6 +785,7 @@ namespace XOX_game
                 siraKimde = 0;
                 oynanmaSayisi = 0;
                 timer1.Start();
+                KazananKontrolEt();
             }
             else
             {
